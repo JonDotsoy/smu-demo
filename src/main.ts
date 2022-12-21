0e16d2bc214720a9d62b1dd2dc55f3496b1b8d3c
Jonathan Delgado <hi@jon.soy>
2022-12-21 16:52:45 -0300

    feat: upgrade main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { S3 } from 'aws-sdk';
+
+const s3 = new S3();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
